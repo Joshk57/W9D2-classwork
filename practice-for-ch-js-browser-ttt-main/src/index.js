82f20feb8d9250114db32c77cadebb2c3b969c9a
jliangrpb <jeremy.liang612@gmail.com>
2023-05-02 17:30:47 -0400

    Made a Board with working hover

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,14 @@
-Add your import statements for View and Game here
+import View from "./ttt-view.js";
+window.View = View;
+import Game from "../ttt_node/game.js";
+window.Game = Game;
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const ng = new Game ();
+  const game = new View (ng, document.getElementsByClassName("ttt")[0]);
+
+});
+
+document.addEventListener('click', () => {
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,11 +1,24 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard()
   }
   
   setupBoard() {
+    const parent = document.createElement("ul");
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++){
+      let child = document.createElement("li");
+      child.dataset.pos = [i, j];
+      parent.append(child);
+    }
+  }
+  this.el.append(parent);
   }
   
   handleClick(e) {
+  
   }
 
   makeMove(square) {
